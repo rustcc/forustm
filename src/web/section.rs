685b897d134e338b8db1e66544d1529ceeb7190d
weaming <garden.yuen@gmail.com>
2017-12-18 07:52:57 +0800

    add fisrt page

--- src/web/section.rs
+++ src/web/section.rs
@@ -1,5 +1,5 @@
 use sapper::{SapperModule, SapperRouter, Response, Request, Result as SapperResult};
-use sapper_std::{Context, render, SessionVal, PathParams};
+use sapper_std::{Context, render, SessionVal, PathParams, QueryParams};
 use super::super::{Redis, Postgresql};
 use super::super::{Section, Articles, RUser};
 use uuid::Uuid;
@@ -16,13 +16,16 @@ impl WebSection {
         let mut web = Context::new();
         let pg_conn = req.ext().get::<Postgresql>().unwrap().get().unwrap();
 
-        let params = get_path_params!(req);
-        let id: Result<Uuid, _> = t_param!(params, "id").clone().parse();
-        if let Err(e) = id {
-            return res_400!(format!("UUID invalid: {}", e));
-        }
+        let path_params = get_path_params!(req);
 
-        let id = id.unwrap();
+        let id: Uuid = match t_param!(path_params, "id").clone().parse() {
+            Ok(i) => i,
+            Err(err) => return res_400!(format!("UUID invalid: {}", err)),
+        };
+        let page = 1i64;
+        web.add("id", &id);
+        web.add("page", &page);
+
         let res = Section::query_with_id(&pg_conn, id);
 
         match res {
@@ -34,11 +37,13 @@ impl WebSection {
 
                 web.add("res", &r);
 
-                let articles = Articles::query_articles_with_section_id(&pg_conn, id);
+                let articles = Articles::query_articles_with_section_id_paging(&pg_conn, id, page);
                 match articles {
                     Ok(arts) => {
                         //println!("articles: {:?}", &arts);
-                        web.add("articles", &arts);
+                        web.add("articles", &arts.articles);
+                        web.add("total", &arts.total);
+                        web.add("max_page", &arts.max_page);
 
                         if let Some(suid) = r.suser {
                             let manager = RUser::query_with_id(&pg_conn, suid).unwrap();
@@ -58,13 +63,16 @@ impl WebSection {
         let mut web = Context::new();
         let pg_conn = req.ext().get::<Postgresql>().unwrap().get().unwrap();
 
-        let params = get_path_params!(req);
-        let id: Result<Uuid, _> = t_param!(params, "id").clone().parse();
-        if let Err(e) = id {
-            return res_400!(format!("UUID invalid: {}", e));
-        }
+        let path_params = get_path_params!(req);
 
-        let id = id.unwrap();
+        let id: Uuid = match t_param!(path_params, "id").clone().parse() {
+            Ok(i) => i,
+            Err(err) => return res_400!(format!("UUID invalid: {}", err)),
+        };
+        let page = 1i64;
+        web.add("id", &id);
+        web.add("page", &page);
+
         let res = Section::query_with_id(&pg_conn, id);
         match res {
             Ok(r) => {
@@ -75,11 +83,13 @@ impl WebSection {
 
                 web.add("res", &r);
 
-                let articles = Articles::query_articles_with_section_id(&pg_conn, id);
+                let articles = Articles::query_articles_with_section_id_paging(&pg_conn, id, page);
                 match articles {
                     Ok(arts) => {
                         //println!("articles: {:?}", &arts);
-                        web.add("articles", &arts);
+                        web.add("articles", &arts.articles);
+                        web.add("total", &arts.total);
+                        web.add("max_page", &arts.max_page);
 
                         if let Some(suid) = r.suser {
                             let manager = RUser::query_with_id(&pg_conn, suid).unwrap();
