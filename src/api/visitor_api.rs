c9d023700b32afab51a059c21ae3b0e067cc9507
piaoliu <441594700@qq.com>
2017-12-19 17:16:30 +0800

    fix typo

--- src/api/visitor_api.rs
+++ src/api/visitor_api.rs
@@ -5,7 +5,7 @@ use serde_json;
 use uuid::Uuid;
 
 use super::super::{LoginUser, RegisteredUser, Redis, Postgresql, RUser};
-use super::super::models::Articles;
+use super::super::Articles;
 
 pub struct Visitor;
 
@@ -110,6 +110,7 @@ impl Visitor {
             }
         };
         res_json!(res)
+    }
 
     fn article_paging(req: &mut Request) -> SapperResult<Response> {
         let pg_pool = req.ext().get::<Postgresql>().unwrap().get().unwrap();
@@ -131,19 +132,19 @@ impl Visitor {
         match Articles::query_articles_with_section_id_paging(&pg_pool, section_id, page) {
             Ok(arts_with_count) => {
                 let res = json!({
-                    "status": true,
-                    "articles": arts_with_count.articles,
-                    "total": arts_with_count.total,
-                    "max_page": arts_with_count.max_page,
-                });
+                "status": true,
+                "articles": arts_with_count.articles,
+                "total": arts_with_count.total,
+                "max_page": arts_with_count.max_page,
+            });
 
                 response.write_body(serde_json::to_string(&res).unwrap());
             },
             Err(err) => {
                 let res = json!({
-                    "status": false,
-                    "error": err,
-                });
+                "status": false,
+                "error": err,
+            });
 
                 response.write_body(serde_json::to_string(&res).unwrap());
             }
@@ -151,6 +152,7 @@ impl Visitor {
         Ok(response)
     }
 }
+
 
 impl SapperModule for Visitor {
     fn router(&self, router: &mut SapperRouter) -> SapperResult<()> {
