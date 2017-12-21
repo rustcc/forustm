df0c267a358f1049fee7302f553d0c51545956fd
piaoliu <441594700@qq.com>
2017-12-21 21:15:41 +0800

    add user home page

--- src/bin/forustm_web.rs
+++ src/bin/forustm_web.rs
@@ -39,6 +39,7 @@ fn main() {
         .add_module(Box::new(Index))
         .add_module(Box::new(WebSection))
         .add_module(Box::new(WebArticle))
+        .add_module(Box::new(Home))
         .static_service(true);
 
     println!("Start listen on http://{}:{}", "127.0.0.1", port);
