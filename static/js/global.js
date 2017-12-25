657736a2a548b77dff54686290d61d058d759101
Daogang Tang <miketang84@outlook.com>
2017-12-25 22:20:03 +0800

    wip.
    Signed-off-by: Daogang Tang <miketang84@outlook.com>

--- static/js/global.js
+++ static/js/global.js
@@ -52,3 +52,23 @@ function checkLoggedIn(run_if_logged_in){
         }
     });
 }
+
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
+Date.prototype.LocalFormat = function () {
+    return new Date(this.getTime() + 3600000 * 8).Format("yyyy-MM-dd hh:mm:ss");
+}
